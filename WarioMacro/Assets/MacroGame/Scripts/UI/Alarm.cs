01454a33e587186ea88169d77698ff761587997a
Rayki <cocoog77@gmail.com>
2022-01-13 21:58:55 +0100

    Alarm linked to UI (placeholder)

--- WarioMacro/Assets/MacroGame/Scripts/UI/Alarm.cs
+++ WarioMacro/Assets/MacroGame/Scripts/UI/Alarm.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class Alarm : MonoBehaviour
 {
+    public static bool isActive { get; private set; }
+    [SerializeField] private PlayableDirector director;
     [Range(0.1f, 15f)]
     [SerializeField] private float increaseOnWin = 2;
     [Range(0.1f, 15f)]
@@ -18,12 +23,33 @@ public class Alarm : MonoBehaviour
 
     public void DecrementCount(bool result)
     {
+        if (isActive) return;
+        
         count -= (result ? increaseOnWin : increaseOnLose) * currentFactor;
-        Debug.Log(count);
+        if (count <= 0)
+        {
+            count = 0;
+            isActive = true;
+            Debug.Log("Alarm mode On");
+        }
+        
+        director.Play();
+        StartCoroutine(PauseAlarm((100 - count) * director.duration / 100));
+        
+    }
+
+    private IEnumerator PauseAlarm(double time)
+    {
+        yield return new WaitForSeconds((float) time);
+
+        director.Pause();
+        director.initialTime = time;
     }
 
     private void Update()
     {
+        if (isActive) return;
+
         currentFactor = 1 + MapManager.floor * floorFactorIncrease
                           + (Ticker.gameBPM - 100) / 10 * bpmFactorIncrease 
                           + (Ticker.difficulty - 1) * difficultyFactorIncrease;
