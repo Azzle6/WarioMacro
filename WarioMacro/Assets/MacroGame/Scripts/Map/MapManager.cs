1c400844d4fff18f8b978bb06b22a593095b9d3c
Rayki <cocoog77@gmail.com>
2022-01-18 23:15:10 +0100

    Added node types previsualisation in Debug log

--- WarioMacro/Assets/MacroGame/Scripts/Map/MapManager.cs
+++ WarioMacro/Assets/MacroGame/Scripts/Map/MapManager.cs
@@ -1,5 +1,7 @@
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using GameTypes;
 using UnityEngine;
 using Random = System.Random;
 
@@ -7,7 +9,8 @@ using Random = System.Random;
 public class MapManager : MonoBehaviour
 {
     public static int floor { get; private set; } = -1;
-    
+    public Dictionary<int, float> typePercentages = new Dictionary<int, float>();
+
     [SerializeField] private Map recruitmentMap;
     [SerializeField] private int mapsPerGame = 5;
     [SerializeField] private GameObject[] mapGOList;
@@ -38,10 +41,79 @@ public class MapManager : MonoBehaviour
         return mapGoQueue.Count == 0;
     }
 
+    private void TypeCountsToPercentages(int total)
+    {
+        int farthestUpperKey = 0;
+        float farthestUpperValue = 1f;
+        int farthestLowerKey = 0;
+        float farthestLowerValue = -1f;
+        int totalPercentage = 0;
+        
+        foreach (int key in typePercentages.Select(pair => pair.Key).ToList())
+        {
+            float v = (float) ((double) typePercentages[key] / total * 20);
+            float distTo05 = v % 1 - 0.5f;
+            
+            if (distTo05 < 0 && distTo05 > farthestLowerValue)
+            {
+                farthestLowerKey = key;
+                farthestLowerValue = distTo05;
+            } 
+            else if (distTo05 >= 0 && distTo05 < farthestUpperValue)
+            {
+                farthestUpperKey = key;
+                farthestUpperValue = distTo05;
+            }
+            
+            typePercentages[key] = Mathf.RoundToInt(v) * 5;
+            totalPercentage += (int) typePercentages[key];
+        }
+
+        if (totalPercentage > 100)
+        {
+            typePercentages[farthestUpperKey] += 100 - totalPercentage;
+        }
+        else if (totalPercentage < 100)
+        {
+            typePercentages[farthestLowerKey] += 100 - totalPercentage;
+        }
+    }
+
     private void OnEnable()
     {
         var rd = new Random();
         mapGoQueue = new Queue<GameObject>(mapGOList.OrderBy(go => rd.Next())
             .Take(mapGOList.Length < mapsPerGame ? mapGOList.Length : mapsPerGame));
+
+        int total = 0;
+        
+        foreach (FieldInfo field in typeof(GameType).GetFields())
+        {
+            typePercentages.Add((int) field.GetValue(null), 0f);
+        }
+
+        foreach (NodeSettings node in mapGoQueue.SelectMany(mapGO =>
+            mapGO.GetComponent<Map>().nodesParent.GetComponentsInChildren<NodeSettings>()))
+        {
+            switch (node.type)
+            {
+                case NodeType.None:
+                    continue;
+                case NodeType.Random:
+                    node.SetRandomType();
+                    break;
+            }
+
+            typePercentages[node.type]++;
+            total++;
+
+        }
+
+        TypeCountsToPercentages(total);
+
+        foreach (var pair in typePercentages)
+        {
+            Debug.Log(pair);
+        }
     }
 }
