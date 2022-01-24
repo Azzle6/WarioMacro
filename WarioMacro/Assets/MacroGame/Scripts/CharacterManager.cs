add04c1f3a68630145a9f0c61d91cd08de8c9a78
Rayki <cocoog77@gmail.com>
2022-01-24 22:15:39 +0100

    Fixed main scene

--- WarioMacro/Assets/MacroGame/Scripts/CharacterManager.cs
+++ WarioMacro/Assets/MacroGame/Scripts/CharacterManager.cs
@@ -1,10 +1,7 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using GameTypes;
 using UnityEngine;
-using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 // ReSharper disable once CheckNamespace
