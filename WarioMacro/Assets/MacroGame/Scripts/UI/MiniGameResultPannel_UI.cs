cb429495a3bd88e766d45d9924a9623be278b861
Guillaume Lambert <guill.lambert@orange.fr>
2022-01-27 20:09:08 +0100

    EndScore UI fix
    display nbMicroGames value

--- WarioMacro/Assets/MacroGame/Scripts/UI/MiniGameResultPannel_UI.cs
+++ WarioMacro/Assets/MacroGame/Scripts/UI/MiniGameResultPannel_UI.cs
@@ -337,6 +337,7 @@ public class MiniGameResultPannel_UI : MonoBehaviour
 
     public IEnumerator CharaApparition(int nodeType)
     {
+
         if(nodeType <= 1) yield break;
 
         Debug.Log(CharacterManager.instance.SpecialistOfTypeInTeam(nodeType));
@@ -353,6 +354,7 @@ public class MiniGameResultPannel_UI : MonoBehaviour
         Debug.Log(selectedChara);
 
         if (selectedChara == null) yield break;
+        GameController.instance.nbMicroGamesSpecialist++;
         AudioManager.MacroPlaySound(selectedChara.GetMGSoundName());
         charaSpecialistSprite.sprite = selectedChara.fullSizeSprite;
         charaApparitionGO.SetActive(true);
